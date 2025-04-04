4e8a6f140e2cc6aa6649483b1782278529df5345
Joel Åkergren <joel.akergren@gmail.com>
2025-04-04 19:54:54 +0200

    Add project files.

---
+++ Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FunGameAPI.Entities
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(Nickname), IsUnique = true)]
+    public class User
+    {
+        public int Id { get; set; }
+
+        public required string Nickname { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+
+        public int NumberOfGames { get; set; } = 0;
+
+        public int NumberOfWins { get; set; } = 0;
+    }
+}
