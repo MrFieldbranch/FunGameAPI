333e8567293340dfe8e06d01d59cf772c40728b0
Joel Åkergren <joel.akergren@gmail.com>
2025-04-29 00:01:55 +0200

    Add temporary MigrationsController

---
+++ Controllers/MigrationsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FunGameAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FunGameAPI.Controllers
+{
+    [ApiController]
+    [Route("migrations")]
+    public class MigrationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MigrationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ApplyMigrations()
+        {
+            await _context.Database.MigrateAsync();
+            return Ok("Migrations applied successfully!");
+        }
+    }
+}
+
