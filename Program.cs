9a3a566ce25ef8d8f471eb687bbe11a88abd650a
Joel Åkergren <joel.akergren@gmail.com>
2025-04-28 22:48:00 +0200

    Optimize Program.cs

--- Program.cs
+++ Program.cs
@@ -5,21 +5,21 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-builder.Services.AddControllers();
+// Services
 builder.Services.AddScoped<RegistrationService>();
 builder.Services.AddScoped<LoginService>();
 builder.Services.AddScoped<UserService>();
 
+// CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigins", builder =>
+    options.AddPolicy("AllowSpecificOrigins", policy =>
     {
-        builder.WithOrigins(
+        policy.WithOrigins(
             "https://hangman-game-la5u.onrender.com",
             "http://localhost:5173"
         )
@@ -28,37 +28,40 @@ builder.Services.AddCors(options =>
     });
 });
 
+// Controllers
+builder.Services.AddControllers();
 
+// Authentication
 builder.Services
-  .AddAuthentication()
-  .AddJwtBearer(options =>
-  {
-      var signingSecret = builder.Configuration["JWT:SigningSecret"]
-      ?? throw new InvalidOperationException("JWT SigningSecret is not configured.");
+    .AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        var signingSecret = builder.Configuration["JWT:SigningSecret"]
+            ?? throw new InvalidOperationException("JWT SigningSecret is not configured.");
 
-      var signingKey = Convert.FromBase64String(signingSecret);
+        var signingKey = Convert.FromBase64String(signingSecret);
 
-      options.TokenValidationParameters = new TokenValidationParameters
-      {
-          ValidateIssuer = false,
-          ValidateAudience = false,
-          ValidateLifetime = false,
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = new SymmetricSecurityKey(signingKey)
-      };
-  });
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(signingKey)
+        };
+    });
 
 var app = builder.Build();
 
+// Important: Middleware order matters!
 app.UseCors("AllowSpecificOrigins");
 
-//app.UseHttpsRedirection();
+// Comment this temporarily if CORS still gives issues
+// app.UseHttpsRedirection();
 
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
-Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
-
